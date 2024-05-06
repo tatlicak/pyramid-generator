3151015dcf76e6f2aa5feb4a4b9547920566b898
Abdullah TATLICAK <tatlicak@gmail.com>
2024-05-06 06:46:11 +0300

    Step 4

--- scripts.js
+++ scripts.js
@@ -1,11 +1,9 @@
 //Step 1 - Declare a character variable in your code.
 //Step 2 - Initialize your character variable by assigning it the value "Hello" during its declaration.
-//Step 3 Change your "Hello" string to use single quotes.
-
+//Step 3 - Change your "Hello" string to use single quotes.
 let character = 'Hello';
 
-//Step 4 Print the value of the character variable to the console. 
+//Step 4 - Print the value of the character variable to the console. 
 //Then, click the "Console" button to view the JavaScript console.
 
 console.log(character);
-
