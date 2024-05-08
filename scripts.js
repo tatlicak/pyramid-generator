c7c774f018415a962e3e6e209d5e8d6703ef4f0f
Abdullah TATLICAK <tatlicak@gmail.com>
2024-05-08 08:01:54 +0300

    Step 11

--- scripts.js
+++ scripts.js
@@ -6,28 +6,28 @@ let character = 'Hello';
 //Step 4 - Print the value of the character variable to the console. 
 //Then, click the "Console" button to view the JavaScript console.
 
-console.log(character);
+
 
 //Step 5 - After your console.log, assign the value "World" to your character variable.
 
-character = "World";
+
 
 //Step 6 - Now log your character variable to the console again.
 
-console.log(character);
+
 
 //Step 7 - Use camel case to declare a new secondCharacter variable.
 
-let secondCharacter;
+
 
 /*Step 9 - Assign the string "Test" to your secondCharacter variable below your declaration. 
 Open the console to see how your log has changed.
 
 secondCharacter = "Test"; */
 
-secondCharacter = character;
+
 
 //Step 8 - Add a console.log() to see what the value of your secondCharacter variable is.
 
-console.log(secondCharacter);
+
 
