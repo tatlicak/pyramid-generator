a527d945417139b319f8cc51b15b07627316bfbc
Abdullah TATLICAK <tatlicak@gmail.com>
2024-06-16 14:31:24 +0300

    Step 24

--- scripts.js
+++ scripts.js
@@ -51,7 +51,3 @@ let rows = ["Naomi", "Quincy","CamperChan"];
 
 console.log(rows[0]);
 
-let cities = ["London", "New York", "Mumbai"]
-console.log(cities);
-cities[cities.length-1] = "Mexico City"
-console.log(cities);
